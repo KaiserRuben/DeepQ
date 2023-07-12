3aa86fff2677cbb6788da668cead6420ec656fc3
Ruben Kaiser <Ruben2412000@gmail.com>
2023-07-12 10:18:55 +0200

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      Test
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonPage } from '@ionic/vue';
+</script>
+
+<style scoped>
+</style>
